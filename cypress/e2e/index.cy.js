4611be08b36652d7ccaf2f7cae313aabed55befd
mizdra <pp.mizdra@gmail.com>
2022-06-27 00:49:26 +0900

    スクリーンショットを追加

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -3,5 +3,20 @@ describe('Index', () => {
     cy.visit('http://localhost:3000')
 
     cy.percySnapshot();
+
+    const expandButton = cy.findByRole('group');
+
+    expandButton.click();
+
+    cy.percySnapshot(`${Cypress.currentTest.titlePath.join(' ')} expandButton`);
+  })
+  it('Capture a index page (expanded)', () => {
+    cy.visit('http://localhost:3000')
+
+    const expandButton = cy.findByRole('group');
+
+    expandButton.click();
+
+    expandButton.percySnapshot();
   })
 })
